aefdf0aeede55c218f119c8e1873261c3ea1e759
Joseph W. Lindley <josephl@tazworks.com>
2016-10-22 17:16:50 -0600

    Added edit actions.

--- lib/ecto_store.ex
+++ lib/ecto_store.ex
@@ -8,13 +8,17 @@ defmodule EctoStore do
       import EctoStore
       require EctoStore.Fetch
       require EctoStore.BuildQueries
+      require EctoStore.Edit
       import EctoStore.Alias
       import Ecto.Query
       alias unquote(repo), as: Repo
 
+      def schema, do: unquote(schema)
+
       EctoStore.Alias.build
       EctoStore.BuildQueries.build(unquote(schema))
       EctoStore.Fetch.build(unquote(repo))
+      EctoStore.Edit.build(unquote(schema), unquote(repo))
     end
   end
 
---
+++ lib/ecto_store/edit.ex
@@ -0,0 +1,62 @@
+defmodule EctoStore.Edit do
+  defmacro build(schema, repo) do
+    quote do
+      def insert(params, changeset \\ :changeset) do
+        default_value = struct unquote(schema), %{}
+        repo = unquote(repo)
+        params = alias_filters(params)
+
+        change = apply(unquote(schema), changeset, [default_value, params])
+        repo.insert change
+      end
+
+      def insert!(params, changeset \\ :changeset) do
+        case insert params, changeset do
+          {:error, reason} -> throw reason
+          {:ok, result} -> result
+        end
+      end
+
+      def update(id_or_model, params, changeset \\ :changeset)
+      def update(id, params, changeset) when is_integer id do
+        repo = unquote(repo)
+        default_value = struct unquote(schema), %{id: id}
+        params = alias_filters(params)
+        change = apply(unquote(schema), changeset, [default_value, params])
+        repo.update change
+      end
+      def update(model, params, changeset) do
+        repo = unquote(repo)
+        params = alias_filters(params)
+
+        change = apply(unquote(schema), changeset, [model, params])
+        repo.update change
+      end
+
+      def update!(id_or_model, params, changeset \\ :changeset)
+      def update!(id, params, changeset) when is_integer id do
+        case update id, params, changeset do
+          {:error, reason} -> throw reason
+          {:ok, result} -> result
+        end
+      end
+      def update!(model, params, changeset) do
+        case update model, params, changeset do
+          {:error, reason} -> throw reason
+          {:ok, result} -> result
+        end
+      end
+
+      def delete(id) when is_integer id do
+        repo = unquote(repo)
+        default_value = struct unquote(schema), %{id: id}
+
+        repo.delete default_value
+      end
+      def delete(model) do
+        repo = unquote(repo)
+        repo.delete model
+      end
+    end
+  end
+end
